261e122c6ffb5e194297abbf52eb487b2a10d6d6
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2023-01-04 15:57:45 +0100

    aggiornamenti file data exploration

--- CreazioneDatasets/dataset_top_clothes_script/exploration_dataset.py
+++ CreazioneDatasets/dataset_top_clothes_script/exploration_dataset.py
@@ -7,30 +7,25 @@ df = pd.read_csv("../newCsv_all_clothes/top_meteo_dataset_selection.csv")
 # descrive il dataset in termini statistici
 print(df.describe())
 
-# costruisce un grafico a barre dove mostra come varia il punteggio rispetto al materiale
-sns.barplot(data=df, x="Materiale", y="Punteggio")
-# plt.show()
-
-# mostra dei grafici a barre dove mostra come varia il punteggio rispetto a ogni feature
+# istogrammi dove mostra come varia il punteggio rispetto a ogni feature
 for label in ["Materiale", "Colore", "Manica", "Stagione", "Meteo", "StagionePrevisione"]:
     sns.barplot(data=df, x=label, y="Punteggio")
-    # plt.show()
+    plt.show()
 
 # mostra un grafico di dispersione dei valori di temperatura rispetto al punteggio
 sns.scatterplot(data=df, x="TemperaturaPercepita", y="Punteggio")
-# plt.show()
+plt.show()
 
-# mostra la forma della distribuzione delle variabili come sopra ma in modo più formale
+# mostra la forma della distribuzione delle variabili come scatterplot ma in modo più formale
 sns.jointplot(data=df, x="TemperaturaPercepita", y="Punteggio")
-# plt.show()
+plt.show()
 
 # mostra grafici dove viene vista la variazione delle varie variabili numeriche
 sns.pairplot(data=df)
-# plt.show()
+plt.show()
 
 # mette in relazione la variabile x e y rispetto ad una colonna specificata --> "Stagione"
 sns.relplot(data=df, x="Punteggio", y="Materiale", kind="line", col="Stagione")
-
-# mostra i grafici creati precedentemente, attenzione a quelli creati nel for
 plt.show()
+
 
