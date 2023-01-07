47ae1a4f643d99eaf8ec438602dcd8f12acab613
frankzamma <frank581.fgz@gmail.com>
2023-01-07 21:50:34 +0100

    Aggiunta classe ShoesML che usa wrapper per creare ML per le scarpe

---
+++ src/main/java/Model/machinelearning/ShoesML.java
@@ -0,0 +1,33 @@
+package Model.machinelearning;
+
+import Model.CapoAbbigliamento;
+import Model.MeteoInformation;
+
+import java.util.List;
+
+public class ShoesML {
+    private RegressionTreeWrapper treeWrapper;
+
+    public ShoesML(){
+        //Creazione e allenamento del regressore
+        treeWrapper =
+                new RegressionTreeWrapper(
+                        "CreazioneDatasets/newCsv_all_clothes/shoes_meteo_dataset_labeled.csv",
+                        false, false);
+    }
+
+    public List<ScoreCapoAbbigliamento> classifyIstances(List<CapoAbbigliamento> list, MeteoInformation meteoInformation,
+                                                         String stagione, boolean getBestThree){
+        List<ScoreCapoAbbigliamento> l = treeWrapper.classifyInstances(list, meteoInformation, stagione);
+
+        if(getBestThree){
+            return treeWrapper.getBestThreeTopClothes(l);
+        }else{
+            return l;
+        }
+    }
+
+
+
+
+}
