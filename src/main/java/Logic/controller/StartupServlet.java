f2600cbc05b38cb24978717ffe1d1be88083f613
frankzamma <frank581.fgz@gmail.com>
2023-01-08 20:17:50 +0100

    Aggiornati i costruttori dei ML e aggiunto la costruzione delle path per dataset e file

--- src/main/java/Logic/controller/StartupServlet.java
+++ src/main/java/Logic/controller/StartupServlet.java
@@ -2,10 +2,14 @@ package Logic.controller;
 
 import Logic.machinelearning.BottomML;
 import Logic.machinelearning.ShoesML;
+import Logic.machinelearning.TopML;
+import Model.Guardaroba;
+import Model.RecoveryGuardaroba;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
 
 @WebServlet(name = "StartupServlet", value = "/StartupServlet", loadOnStartup = 1)
@@ -13,14 +17,32 @@ public class StartupServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        //Il costruttore crea e allena il ML
-        ShoesML shoesML =  new ShoesML();
-        BottomML bottomML = new BottomML();
-        //TODO Aggiungere gli altri ML
+        //Path dei dataset
+        String shoesDataset = this.getServletContext().getRealPath("") + File.separator +
+                "WEB-INF/resources/csv/shoes_meteo_dataset_labeled.csv";
+        String bottomDataset = this.getServletContext().getRealPath("") + File.separator +
+                "WEB-INF/resources/csv/bottom_meteo_dataset_labeled.csv";
+
+        String topDataset = this.getServletContext().getRealPath("") + File.separator +
+                "WEB-INF/resources/csv/top_meteo_dataset_labeled.csv";
+
+        //Creazione dei ML (Durante la creazione si effettua anche l'addestramento)
+        ShoesML shoesML =  new ShoesML(shoesDataset);
+        BottomML bottomML = new BottomML(bottomDataset);
+        TopML topML = new TopML(topDataset);
+
+        // Caricamento del guardaroba d'esempio
+        RecoveryGuardaroba recoveryGuardaroba =
+                new RecoveryGuardaroba(this.getServletContext()
+                        .getRealPath(""  + File.separator + "WEB-INF/resources/CapoAbbigliamentoList" ));
+        Guardaroba guardaroba = recoveryGuardaroba.getGuardaroba();
+
 
         ServletContext context = this.getServletContext();
         context.setAttribute("shoesML", shoesML);
         context.setAttribute("bottomML", bottomML);
+        context.setAttribute("topML", topML);
+        context.setAttribute("guardaroba_context", guardaroba);
 
 
     }
