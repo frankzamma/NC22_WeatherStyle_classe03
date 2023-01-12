7c04b2bf2c9882c7a63cdd2502c9c2aed39baf91
frankzamma <frank581.fgz@gmail.com>
2023-01-12 17:01:31 +0100

    Terminata data understanding scarpe e meteo

--- CreazioneDatasets/dataset_shoes/explorer_dataset_shoes.py
+++ CreazioneDatasets/dataset_shoes/explorer_dataset_shoes.py
@@ -2,7 +2,7 @@ import seaborn as sb
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('../csv_all_clothes/shoes-dataset.csv')
-
+df_new = pd.read_csv('../newCsv_all_clothes/shoes-dataset.csv')
 print(df["CID"].count())
 df.dropna(inplace=True)
 print(df["CID"].count())
@@ -13,6 +13,8 @@ print(df["HeelHeight"].value_counts())
 
 # sb.countplot(data=df, y=df['Category'])
 # sb.countplot(data=df, y=df['SubCategory'])
-sb.countplot(data=df, y=df['Insole'])
+# sb.countplot(data=df, y=df['Insole'])
+sb.set(rc={"figure.figsize":(10, 6)})
+sb.countplot(data=df_new, x=df_new['Tipo'])
 
 plt.show()
