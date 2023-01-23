b9df08b18647d0098e486bf564e9ae12e0a4efda
angelo palmieri <palmieri309@gmail.com>
2023-01-23 17:22:57 +0100

    Inserite le tabelle Guardaroba, Amministratore e RichiestaPromozione nel file CreateDB.sql

--- Database/CreateDB.sql
+++ Database/CreateDB.sql
@@ -2,6 +2,8 @@ drop database if exists weatherstyle;
 
 CREATE DATABASE weatherstyle;
 
+use weatherstyle;
+
 /*
     Raffaele
     UTENTE (ID, nome, cognome, dataNascita, cittaNascita, IDguardaroba*)
@@ -17,8 +19,36 @@ CREATE DATABASE weatherstyle;
 
     Angelo
     GUARDAROBA (ID, nome, numeroCapi)
-    RICHIESTAPROMOZIONE (ID, tematiche, esperienze, IDutente, IDadmin*)
+    RICHIESTAPROMOZIONE (ID, tematiche, esperienze, IDutente*, IDadmin*)
     ADMIN (ID, nome, cognome)
+*/
+    CREATE TABLE Guardaroba (
+        ID INT PRIMARY KEY,
+        nome VARCHAR(30) NOT NULL,
+        numeroCapi INT NOT NULL
+    );
+
+    CREATE TABLE Amministratore (
+        ID INT PRIMARY KEY,
+        nome VARCHAR(30) NOT NULL,
+        cognome VARCHAR(30) NOT NULL,
+    );
+
+    CREATE TABLE RichiestaPromozione (
+        ID INT PRIMARY KEY,
+        tematiche VARCHAR(50) NOT NULL,
+        esperienze VARCHAR(250) NOT NULL,
+        stato VARCHAR(15) NOT NULL,
+        FOREIGN KEY (IDutente)
+            REFERENCES Utente(ID)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE,
+        FOREIGN KEY (IDadmin)
+            REFERENCES Amministratore(ID)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
+    );
+/*
 
     Francesco
     POST (ID, data, titolo, descrizione, IDutente*)
