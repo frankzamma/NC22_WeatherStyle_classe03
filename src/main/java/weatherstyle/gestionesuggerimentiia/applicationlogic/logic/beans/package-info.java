5f661ec2ac040f4a30bc6b4a938696ee310287fe
frankzamma <frank581.fgz@gmail.com>
2023-01-26 00:23:42 +0100

    Aggiunti package-info.java

---
+++ src/main/java/weatherstyle/gestionesuggerimentiia/applicationlogic/logic/beans/package-info.java
@@ -0,0 +1,1 @@
+package weatherstyle.gestionesuggerimentiia.applicationlogic.logic.beans;
--- src/main/java/weatherstyle/gestioneutenti/applicationlogic/logic/beans/package-info.java
+++ src/main/java/weatherstyle/gestioneutenti/applicationlogic/logic/beans/package-info.java
@@ -1,4 +1,1 @@
 package weatherstyle.gestioneutenti.applicationlogic.logic.beans;
-
-public class DaTogliere {
-}
