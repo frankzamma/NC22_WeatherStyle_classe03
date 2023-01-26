772a5be87e88086fa28cff94349d6d7007abfca4
frankzamma <frank581.fgz@gmail.com>
2023-01-27 00:08:26 +0100

    Implementati metodi e aggiornata Interfaccia

--- src/main/java/weatherstyle/gestioneutenti/applicationlogic/logic/service/UtenteLogicService.java
+++ src/main/java/weatherstyle/gestioneutenti/applicationlogic/logic/service/UtenteLogicService.java
@@ -1,16 +1,99 @@
 package weatherstyle.gestioneutenti.applicationlogic.logic.service;
 
+import weatherstyle.gestionecitta.applicationlogic.logic.beans.Citta;
 import weatherstyle.gestioneutenti.applicationlogic.logic.beans.Utente;
+import weatherstyle.gestioneutenti.storage.dao.UtenteDAOImpl;
+import weatherstyle.gestioneutenti.storage.dao.UtenteDAOInterface;
+import weatherstyle.utils.ErrorParameterException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UtenteLogicService implements UtenteLogicServiceInterface{
-    @Override
-    public boolean registraUtente(Utente utente) {
-        return false;
+    private UtenteDAOInterface utenteDAO;
+
+    public UtenteLogicService() {
+        utenteDAO = new UtenteDAOImpl();
+    }
+
+    public UtenteLogicService(UtenteDAOInterface utenteDAO) {
+        this.utenteDAO = utenteDAO;
     }
 
     @Override
-    public Utente loginUtente(String username, String password) {
-        return null;
+    public Utente registraUtente(String nome, String cognome, LocalDate dataNascita, String email, String password, Citta citta) throws ErrorParameterException{
+            List<String> errorParameter =  new ArrayList<>();
+
+        if (email==null || (!email.matches("^[a-z0-9\\.\\_]+@[a-z]+\\.[a-z]{2,3}$")))
+            errorParameter.add("email");
+
+        Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!_;,:+.-]).{8,20}$");
+        Matcher matcher = pattern.matcher(password);
+        if (password==null || !matcher.matches())
+            errorParameter.add("password");
+
+        if (nome==null || nome.length()<3 || !nome.matches("^[A-Za-z\\s]{3,30}$"))
+            errorParameter.add("nome");
+
+        if (cognome==null || cognome.length()<3 || !cognome.matches("^[A-Za-z\\s]{3,30}$"))
+            errorParameter.add("cognome");
+
+
+        LocalDate today =  LocalDate.now();
+
+        if(!dataNascita.isBefore(today)){
+            errorParameter.add("data-nascita");
+        }
+
+
+        if(errorParameter.isEmpty()){
+            Utente u = new Utente();
+
+            u.setNome(nome);
+            u.setCognome(cognome);
+            u.setPassword(password);
+            u.setDataNascita(dataNascita);
+            u.setEcologista(false);
+            u.setEmail(email);
+            List<Citta> cittaList =  new ArrayList<>();
+            if(citta != null){
+                cittaList.add(citta);
+            }
+            u.setCitta(cittaList);
+
+            utenteDAO.doSaveUtente(u);
+
+            return u;
+        }else{
+            throw new ErrorParameterException(errorParameter);
+        }
+    }
+
+    @Override
+    public Utente loginUtente(String email, String password) {
+
+        if(email == null && password == null){
+            Pattern pattern =  Pattern.compile("^[a-z0-9\\.\\_]+@[a-z]+\\.[a-z]{2,3}$");
+            Matcher matcher = pattern.matcher(email);
+
+            if(matcher.matches()){
+                Utente u = utenteDAO.doRetrieveUtenteByUsernameAndPassword(email, password);
+
+                if(u != null){
+                    return u;
+                }else{
+                    throw new IllegalArgumentException("Email e/o password non corretti");
+                }
+            }else{
+                throw new IllegalArgumentException("Formato email non corretto");
+            }
+        }else{
+            throw new IllegalArgumentException("Email e password richiesti per accedere!");
+        }
     }
 
     @Override
@@ -20,6 +103,10 @@ public class UtenteLogicService implements UtenteLogicServiceInterface{
 
     @Override
     public boolean existsUtente(String email) {
-        return false;
+        if (email==null || (!email.matches("^[a-z0-9\\.\\_]+@[a-z]+\\.[a-z]{2,3}$"))){
+            return utenteDAO.doExistsEmail(email);
+        }else{
+            throw new IllegalArgumentException("Formato Email non corretto");
+        }
     }
 }
--- src/main/java/weatherstyle/gestioneutenti/applicationlogic/logic/service/UtenteLogicServiceInterface.java
+++ src/main/java/weatherstyle/gestioneutenti/applicationlogic/logic/service/UtenteLogicServiceInterface.java
@@ -1,10 +1,15 @@
 package weatherstyle.gestioneutenti.applicationlogic.logic.service;
 
+import weatherstyle.gestionecitta.applicationlogic.logic.beans.Citta;
 import weatherstyle.gestioneutenti.applicationlogic.logic.beans.Utente;
+import weatherstyle.utils.ErrorParameterException;
+
+import java.time.LocalDate;
 
 public interface UtenteLogicServiceInterface {
 
-    boolean registraUtente(Utente utente);
+    Utente registraUtente(String nome, String cognome, LocalDate dataNascita,
+                          String email, String password, Citta citta)  throws ErrorParameterException;
     Utente loginUtente(String username, String password);
     void promuoviUtenteAdEcologista(Utente utente);
     boolean existsUtente(String email);
