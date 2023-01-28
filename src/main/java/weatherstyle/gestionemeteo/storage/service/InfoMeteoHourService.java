43c5ccfdb9bfabb91a80bf5da8f1ea37a90285d1
frankzamma <frank581.fgz@gmail.com>
2023-01-28 01:01:40 +0100

    Implementata service Meteo Hour

--- src/main/java/weatherstyle/gestionemeteo/storage/service/InfoMeteoHourService.java
+++ src/main/java/weatherstyle/gestionemeteo/storage/service/InfoMeteoHourService.java
@@ -2,11 +2,12 @@ package weatherstyle.gestionemeteo.storage.service;
 
 import weatherstyle.gestionecitta.applicationlogic.logic.beans.Citta;
 import weatherstyle.gestionemeteo.applicationlogic.logic.beans.MeteoHour;
+import weatherstyle.gestionemeteo.applicationlogic.logic.beans.MeteoHours;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface InfoMeteoHourService {
-      MeteoHour getInfoMeteoHourByDay(LocalDate day, LocalDate citta);
-      List<MeteoHour> getInfoMeteoHourByRangeOfDays(LocalDate init, LocalDate end, Citta citta);
+      MeteoHours getInfoMeteoHourByDay(LocalDate day, Citta citta);
+      List<MeteoHours> getInfoMeteoHourByRangeOfDays(LocalDate init, LocalDate end, Citta citta);
 }
