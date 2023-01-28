64b458ff6e7ba6dfdcf76f23018fec3b8e14220b
migli <miglinoannalaura@gmail.com>
2023-01-28 12:58:41 +0100

    Aggiunta possibilità di inserire un'immagine; aggiunti relativi controlli

--- src/main/java/weatherstyle/gestioneguardaroba/applicationlogic/control/InserisciCapo.java
+++ src/main/java/weatherstyle/gestioneguardaroba/applicationlogic/control/InserisciCapo.java
@@ -10,10 +10,14 @@ import weatherstyle.gestioneguardaroba.applicationlogic.logic.beans.Scarpe;
 import weatherstyle.gestioneguardaroba.applicationlogic.logic.service.CapoAbbigliamentoService;
 import weatherstyle.utils.ErrorParameterException;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+        maxFileSize = 1024 * 1024 * 10,
+        maxRequestSize = 1024 * 1024 * 5 * 5)
 @WebServlet(name = "InserisciCapo", value = "/inserisci-capo")
 public class InserisciCapo extends HttpServlet {
     @Override
@@ -30,74 +34,106 @@ public class InserisciCapo extends HttpServlet {
 
         String tipoCapo = request.getParameter("tipologia");
         if ((tipoCapo==null) || (tipoCapo.equals(""))){
-            request.setAttribute("errore tipo", "erroreTipo");
-            response.sendRedirect("loadCapoAbbigliamento.jsp");
+            request.setAttribute("message", "Selezionare un tipo di capo d'abbigliamento");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("loadCapoAbbigliamento.jsp");
+            dispatcher.forward(request,response);
         }
 
-
         String nomeCapo = request.getParameter("nomeCapo");
-        String directoryCapo = request.getParameter("directoryCapo");
+
         String colore = request.getParameter("colore");
         String stagione = request.getParameter("stagione");
 
-
+        String manica = null;
+        String materialeM = null;
         if ("maglia".equals(tipoCapo)) {
-            String materiale = request.getParameter("materiale");
-            String manica = request.getParameter("manica");
-
-            Maglia m = new Maglia(nomeCapo, directoryCapo, stagione,colore,manica, materiale);
-            try {
-                service.salvaMaglia(m);
-            } catch (ErrorParameterException e) {
-                errorService = e.getErrorParameter();
-            }
+            materialeM = request.getParameter("materiale");
+            manica = request.getParameter("manica");
         }
 
+        String materialeP = null;
+        String lunghezza = null;
         if ("pantaloni".equals(tipoCapo)) {
-            String materiale = request.getParameter("materiale");
-            String lunghezza = request.getParameter("lungPantalone");
-
-            Pantaloni p = new Pantaloni(nomeCapo, directoryCapo, stagione, colore, lunghezza, materiale);
-            try {
-                service.salvaPantaloni(p);
-            } catch (ErrorParameterException e) {
-                errorService = e.getErrorParameter();
-            }
+            materialeP= request.getParameter("materiale");
+            lunghezza = request.getParameter("lungPantalone");
         }
 
+        String tipo = null;
+        boolean scivol = false;
+        boolean imper = false;
         if ("scarpe".equals(tipoCapo)) {
-            String tipo = request.getParameter("tipoScarpa");
+            tipo = request.getParameter("tipoScarpa");
             String scivoloso = request.getParameter("scivoloso");
-            boolean scivol;
-            if ("scivsi".equals(scivoloso)) {
+            if ("scivsi".equals(scivoloso))
                 scivol = true;
-            } else {
-                scivol = false;
-            }
+
             String impermeabile = request.getParameter("impermeabile");
-            boolean imper;
-            if ("impsi".equals(impermeabile)) {
+            if ("impsi".equals(impermeabile))
                 imper = true;
-            } else {
-                imper = false;
-            }
-            Scarpe s = new Scarpe(nomeCapo, directoryCapo, stagione, colore, tipo, scivol, imper);
-            try {
-                service.salvaScarpe(s);
-            } catch (ErrorParameterException e) {
-                errorService = e.getErrorParameter();
-            }
+
         }
 
-        if (errorService.isEmpty())
-            address = "visualizzaGuardaroba.jsp";
+        String[] tmp = {"jpg", "png"};
+        List<String> extensions = Arrays.asList(tmp);
+        Part foto = request.getPart("foto");
+        String fileName = foto.getSubmittedFileName();
+        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
 
-        else{
-            request.setAttribute("errorListService", errorService);
-            address = "loadCapoAbbigliamento.jsp";
+        if (!extensions.contains(extension)){
+            request.setAttribute("message", "Errore formato immagine");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("loadCapoAbbigliamento.jsp");
+            dispatcher.forward(request,response);
+
+        }else{
+            String path = request.getServletContext().getRealPath("") + "images";
+
+            File pathNew = new File(path);
+
+            //creo la cartella
+            if (!pathNew.exists()) {
+                pathNew.mkdir();
+            }
+
+            String extendendPath =  path + File.separator + nomeCapo + tipoCapo;
+            File fileExtendendPath =  new File(extendendPath);
+
+            if(!fileExtendendPath.exists())
+                fileExtendendPath.mkdir();
+
+            if ("maglia".equals(tipoCapo)){
+                Maglia m = new Maglia(nomeCapo, extendendPath, stagione,colore, manica, materialeM);
+                try {
+                    service.salvaMaglia(m);
+                } catch (ErrorParameterException e) {
+                    errorService = e.getErrorParameter();
+                }
+            }
+            if ("pantaloni".equals(tipoCapo)){
+                Pantaloni p = new Pantaloni(nomeCapo, extendendPath, stagione, colore, lunghezza, materialeP);
+                try {
+                    service.salvaPantaloni(p);
+                } catch (ErrorParameterException e) {
+                    errorService = e.getErrorParameter();
+                }
+            }
+            if ("scarpe".equals(tipoCapo)){
+                Scarpe s = new Scarpe(nomeCapo, extendendPath, stagione, colore, tipo, scivol, imper);
+                try {
+                    service.salvaScarpe(s);
+                } catch (ErrorParameterException e) {
+                    errorService = e.getErrorParameter();
+                }
+            }
+
+            if (errorService.isEmpty())
+                address = "visualizzaGuardaroba.jsp";
+            else{
+                request.setAttribute("errorListService", errorService);
+                address = "loadCapoAbbigliamento.jsp";
+            }
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher(address);
+            dispatcher.forward(request,response);
         }
-
-        RequestDispatcher dispatcher = request.getRequestDispatcher(address);
-        dispatcher.forward(request,response);
     }
 }
