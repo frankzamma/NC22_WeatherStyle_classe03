c48c3cfe938eb7f9692b74ae83f987b98147b796
angelo palmieri <palmieri309@gmail.com>
2023-01-29 20:30:44 +0100

    Rifiniture riguardo l'area personale dell'utente.

--- src/main/java/weatherstyle/gestioneutenti/applicationlogic/control/AreaPersonaleServlet.java
+++ src/main/java/weatherstyle/gestioneutenti/applicationlogic/control/AreaPersonaleServlet.java
@@ -3,6 +3,10 @@ package weatherstyle.gestioneutenti.applicationlogic.control;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import weatherstyle.gestioneambiente.applicationlogic.logic.beans.RichiestaPromozione;
+import weatherstyle.gestioneambiente.applicationlogic.logic.service.RichiestaPromozioneLogicImpl;
+import weatherstyle.gestioneambiente.applicationlogic.logic.service.RichiestaPromozioneLogicInterface;
+import weatherstyle.gestioneutenti.applicationlogic.logic.beans.Utente;
 
 import java.io.IOException;
 
